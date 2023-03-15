03eb0423bbc5920ffe8c37645a526c4a9d6ac805
anwugos <65577106+anwugos@users.noreply.github.com>
2023-03-15 16:21:49 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -13,3 +13,8 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-se
 
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
+# argon主题
+find . -maxdepth 4 -iname "*argon*" -path "*/luci/*" -type d | xargs rm -rf
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+sed -i 's/"Argon 主题设置"/"主题设置"/g' `grep "Argon 主题设置" -rl ./`
